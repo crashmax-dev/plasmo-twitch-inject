0bc4ce8f531fe53ce967f065610fd1304764fdf5
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-01 21:48:29 +0800

    upd

--- content.tsx
+++ content.tsx
@@ -1,3 +1,4 @@
+import { createRoot } from 'react-dom/client'
 import cssText from 'data-text:~style.css'
 import { waitElement } from '@1natsu/wait-element'
 import type { PlasmoContentScript } from 'plasmo'
@@ -13,18 +14,39 @@ export const getStyle = () => {
   return style
 }
 
-export const getMountPoint = async () => {
-  const el = await waitElement('h2.CoreText-sc-cpl358-0')
-  const div = document.createElement('div')
-  div.classList.add('title')
-  el.appendChild(div)
-  return div
+export const getRootContainer = () => {
+  return document.querySelector('.tw-modal-header__title > h2.tw-title')
 }
+
+async function observeElement(selector: string, callback: (el: Element) => void) {
+  const observer = new MutationObserver((mutation) => {
+    mutation.forEach((mutation) => {
+      mutation.addedNodes.forEach((el) => {
+        const addedEl = (el as Element).querySelector(selector)
+        callback(addedEl)
+      })
+    })
+  })
+
+  const el = await waitElement(selector)
+  if (el) {
+    observer.observe(el, {
+      childList: true,
+      subtree: true
+    })
+  }
+}
+
+window.addEventListener('load', async () => {
+  const el = await waitElement('.tw-modal-header__title > h2.tw-title')
+  const root = createRoot(el)
+  root.render(<Hello />)
+})
 
 const Hello = () => {
   return (
     <div className='title'>
-      <h1 style={{ color: 'tomato', zIndex: 999 }}>Привет, Виталь!</h1>
+      <h1 style={{ color: 'tomato' }}>Привет, Виталь!</h1>
     </div>
   )
 }
